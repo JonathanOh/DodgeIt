4880975a7b91e8f08a0c723b5720b10f264ce9a9
Jonathan Oh <jonoh@yelp.com>
2017-10-18 17:06:11 -0700

    added squareview generation

--- DodgeIt/Views/GridContainerView.swift
+++ DodgeIt/Views/GridContainerView.swift
@@ -10,12 +10,26 @@ import UIKit
 
 class GridContainerView: UIView {
     let currentPuzzle: Puzzle
+    let squareViewMatrix: [[SquareView]]
     
     init(currentPuzzle: Puzzle) {
         self.currentPuzzle = currentPuzzle
+        self.squareViewMatrix = GridContainerView.generateSquareViews(currentPuzzle: currentPuzzle)
         super.init(frame: .zero)
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = .black
+    }
+    
+    static func generateSquareViews(currentPuzzle: Puzzle) -> [[SquareView]] {
+        var squareViews = [[SquareView]]()
+        for y in 0..<currentPuzzle.numberOfCellsInHeight {
+            var squareRow = [SquareView]()
+            for x in 0..<currentPuzzle.numberOfCellsInWidth {
+                squareRow.append(SquareView(currentPuzzle: currentPuzzle, location: (x, y)))
+            }
+            squareViews.append(squareRow)
+        }
+        return squareViews
     }
     
     //Create a 2D matrix of SquareViews
