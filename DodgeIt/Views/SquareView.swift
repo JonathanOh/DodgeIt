07cc643c282f961a7b0deebe2c34f9886f817924
Jonathan Oh <jonoh@yelp.com>
2017-10-25 13:49:29 -0700

    added slightly better animation to eyes

--- DodgeIt/Views/SquareView.swift
+++ DodgeIt/Views/SquareView.swift
@@ -9,11 +9,11 @@
 import UIKit
 import QuartzCore
 
-class SquareView: UIImageView, CAAnimationDelegate {
+class SquareView: UIView, CAAnimationDelegate {
     let currentPuzzle: Puzzle
     let location: (Int, Int)
     let locationStringValue: String
-    let explosionSheet = UIImage(imageLiteralResourceName: "explosion7")
+    let explosionSheet = UIImage(imageLiteralResourceName: "explosion3")
     
     let puzzleBackgroundColor: UIColor = UIColor(red: 1, green: 1, blue: 100.0/255.0, alpha: 1)
     let explosionColor: UIColor = .black//UIColor(red: 1.0, green: 50.0/255.0, blue: 50.0/255.0, alpha: 1)//(255,179,186)
@@ -23,6 +23,7 @@ class SquareView: UIImageView, CAAnimationDelegate {
         self.currentPuzzle = currentPuzzle
         self.location = location
         self.locationStringValue = "\(location.0),\(location.1)"
+        self.explosionImageView = UIImageView(image: explosionSheet)
         super.init(frame: .zero)
         backgroundColor = checkIfObstacle() ? obstacleColor : puzzleBackgroundColor
         if checkIfSafeHaven() { backgroundColor = UIColor(red: 100/255.0, green: 1.0, blue: 100/255.0, alpha: 1) }
@@ -44,9 +45,9 @@ class SquareView: UIImageView, CAAnimationDelegate {
         if checkIfObstacle() { return }
         if checkIfSafeHaven() { return }
         backgroundColor = explosionColor
-        UIView.animate(withDuration: 0.75) { [weak self] in
+        UIView.animate(withDuration: 0.6, delay: 0.1, options: [UIViewAnimationOptions.allowUserInteraction], animations: { [weak self] in
             self?.backgroundColor = self?.puzzleBackgroundColor
-        }
+        }, completion: nil)
     }
     
 }
