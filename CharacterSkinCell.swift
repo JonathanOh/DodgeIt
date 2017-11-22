bd3aec22f2ea53bbc546f1295ae2a2e5f7aaca31
Jonathan Oh <jonathanohs@gmail.com>
2017-11-22 02:00:53 -0800

    added gem icon to gem purchase buttons

--- CharacterSkinCell.swift
+++ CharacterSkinCell.swift
@@ -70,16 +70,16 @@ class CharacterSkinCell: UITableViewCell {
         
         if doesUserOwnSkin {
             let skinIsCurrentlySelected = PlayerDefaults.shared.getSelectedSkinID() == character.character_id
-            setButton.setupButton(title: skinIsCurrentlySelected ? "Selected" : "Set", colorOfBackground: skinIsCurrentlySelected ? .gray : buttonColors, target: self, action: #selector(didTapSetButton))
+            setButton.setupButton(title: skinIsCurrentlySelected ? "Selected" : "Set", colorOfBackground: skinIsCurrentlySelected ? .gray : buttonColors, target: self, action: #selector(didTapSetButton), displayGem: false)
             addSubview(setButton)
             setButton.rightAnchor.constraint(equalTo: rightAnchor, constant: -35).isActive = true
             setButton.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
             setButton.heightAnchor.constraint(equalToConstant: 45).isActive = true
             setButton.widthAnchor.constraint(equalToConstant: 125).isActive = true
         } else {
-            moneyPurchaseButton.setupButton(title: character.moneyCost, colorOfBackground: buttonColors, target: self, action: #selector(didTapRealMoneyPurchaseButton))
+            moneyPurchaseButton.setupButton(title: character.moneyCost, colorOfBackground: buttonColors, target: self, action: #selector(didTapRealMoneyPurchaseButton), displayGem: false)
             addSubview(moneyPurchaseButton)
-            coinPurchaseButton.setupButton(title: character.coinCost.getCommaFormattedNumberToString(), colorOfBackground: buttonColors, target: self, action: #selector(didTapCoinPurchaseButton))
+            coinPurchaseButton.setupButton(title: character.coinCost.getCommaFormattedNumberToString(), colorOfBackground: buttonColors, target: self, action: #selector(didTapCoinPurchaseButton), displayGem: true)
             addSubview(coinPurchaseButton)
             
             let buttonStackView = UIStackView(arrangedSubviews: [moneyPurchaseButton, coinPurchaseButton])
@@ -112,13 +112,26 @@ class CharacterSkinCellButton: UIButton {
     init() {
         super.init(frame: .zero)
     }
-    func setupButton(title: String = "", colorOfBackground: UIColor?, target: Any?, action: Selector) {
+    func setupButton(title: String = "", colorOfBackground: UIColor?, target: Any?, action: Selector, displayGem: Bool) {
         translatesAutoresizingMaskIntoConstraints = false
         setTitle(title, for: .normal)
         showsTouchWhenHighlighted = true
         backgroundColor = colorOfBackground
         layer.cornerRadius = 5
         addTarget(target, action: action, for: .touchUpInside)
+        
+        if displayGem {
+            let coinImageView = UIImageView(image: UIImage(imageLiteralResourceName: "yellow_gem"))
+            coinImageView.contentMode = .scaleAspectFit
+            coinImageView.translatesAutoresizingMaskIntoConstraints = false
+            addSubview(coinImageView)
+            coinImageView.widthAnchor.constraint(equalTo: heightAnchor, multiplier: 0.5).isActive = true
+            coinImageView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.5).isActive = true
+            //coinImageView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+            coinImageView.rightAnchor.constraint(equalTo: rightAnchor, constant: -15).isActive = true
+            coinImageView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+            
+        }
     }
     
     required init?(coder aDecoder: NSCoder) {
